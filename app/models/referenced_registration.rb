3be492bc078cf9b8a57f06fe7ef0d179387168f7
Ioannis Kousis <bonobo@otenet.gr>
2021-07-06 09:33:08 +0300

    model User added relationships and validations

--- app/models/referenced_registration.rb
+++ app/models/referenced_registration.rb
@@ -1,4 +1,9 @@
 class ReferencedRegistration < ApplicationRecord
+  # when a new user registers using a referral code
+  # then 
+  # a record is created to link the inviter user (the creator of the refarral code) with the new user
+
   belongs_to :user
-  belongs_to :user
+  belongs_to :referer, class_name: 'User', foreign_key: 'referer_id'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true, length: { minimum: 4 }
+  validates :password, presence: true, length: { minimum: 4 }
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+
+
+  # points to referrals table. added when user creates a new referral code
+  has_one :referral, dependent: :destroy
+
+  # points to referenced_registrations table
+  has_many :referenced_registrations, foreign_key: 'referer_id', dependent: :destroy
+  has_many :invited_users, class_name: 'User', through: :referenced_registrations, source: :user
   
+  # added when registers as a new user with referral code
+  has_one :inverse_referenced_registration, class_name: 'ReferencedRegistration', foreign_key: 'user_id', dependent: :destroy
+
+  has_one :inviter, class_name: 'User', through: :inverse_referenced_registration, foreign_key: 'referer_id', source: :referer
+
 end
